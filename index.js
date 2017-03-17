4af1731c1043ef3c69b1ccc7536132bef5599a43
mldangelo <michael.l.dangelo@gmail.com>
2017-03-16 23:17:50 -0700

    improved scraping script

--- index.js
+++ index.js
@@ -1,86 +1,50 @@
-import fs from 'fs';
 import vo from 'vo';
 import Nightmare from 'nightmare';
+import tags from './tags';
+
 const nightmare = Nightmare({
   show: true,
   waitTimeout: 100000 // in ms
 });
 
-// https://www.instagram.com/explore/tags/europeanshorthaircat/
-
-const tags = [
-  'abyssinian',
-  'abyssiniancat',
-  'aegeancat',
-  'americanbobtail',
-  'americanbobtailcat',
-  'americancurl',
-  'americancurlcat',
-  'americanshorthair',
-  'americanwirehair',
-  'americanwirehaircat',
-  'arabianmau',
-  'arabianmaucat',
-  'asiansemilonghair',
-  'asiancat',
-  'australianmist',
-  'balinesecat',
-  'bambinocat',
-  'bengalcat',
-  'birmancat',
-  'bombaycat',
-  'brazilianshorthair',
-  'britishlonghair',
-  'britishsemilonghair',
-  'britishshorthair',
-  'burmesecat',
-  'burmillacat',
-  'californiaspangled',
-  'chantillytiffany',
-  'chartreuxcat',
-  'chausie',
-  'cheetohcat',
-  'colorpointshorthair',
-  'cornishrex',
-  'cymric',
-  'cypruscat',
-  'devonrexcat',
-  'donskoysphynx',
-  'dwarfcat',
-  'egyptianmau',
-  'europeanshorthaircat',
-  'exoticshorthaircats',
-];
-
 const run = function*() {
+  
+  // Login here
   yield nightmare
     .goto('https://instagram.com/')
-    .wait(60000);
+    .wait(90000);
 
   for (let i = 0; i < tags.length; i++) {
     let tag = tags[i];
-
+    console.log(`Starting: ${i + 1}/${tags.length} ${tag}`);
     yield nightmare
       .goto(`https://www.instagram.com/explore/tags/${tag}/`)
       .wait(2000);
 
     yield nightmare.evaluate(function() {
       var links = document.getElementsByTagName('a');
-      var more = Array.prototype.filter.call(links, function(n) {
+      var buttons = Array.prototype.filter.call(links, function(n) {
         return n.innerHTML.toLowerCase() === 'load more';
-      })[0];
-      more.click();
+      });
+      if (buttons.length === 1) {
+        buttons[0].click();
+      }
     });
+    
     let previousHeight = 0;
     let currentHeight = -1;
+    
     while (previousHeight !== currentHeight && currentHeight < 200000) {
-      console.log('scrolling', previousHeight, currentHeight);
+      console.log(tag, 'scrolling', previousHeight, currentHeight);
       previousHeight = currentHeight;
       currentHeight = yield nightmare.evaluate(() => document.body.scrollHeight);
       yield nightmare.scrollTo(currentHeight, 0)
         .wait(600);
     }
+    
     yield nightmare.html(`./pages/${tag}`, 'HTMLComplete');
+    yield nightmare.wait(15000);
+    
   }
   yield nightmare.end();
   yield nightmare.then(() => console.log('Finished save')).catch(error => {
@@ -89,6 +53,6 @@ const run = function*() {
 };
 
 vo(run)(err => {
-  console.dir(err);
-  console.log('done');
+  if (err) console.dir(err);
+  console.log(`finished downloading ${tags.length} images.`);
 });
